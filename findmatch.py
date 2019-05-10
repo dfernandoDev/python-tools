c1108cd95e95c7f2041b08a911e181df59e846be
Dinesh Fernando <29702667+dfernandoDev@users.noreply.github.com>
2019-05-10 11:15:34 -0500

    search for matching data based on regex

---
+++ findmatch.py
@@ -0,0 +1,19 @@
+import json
+import re
+
+# read json output from
+# https://github.com/simplifi/rebeu/blob/master/datadog/scripts/sifi_datadog_search.py
+with open('monitors.json', 'r') as fp:
+    jsondata = json.load(fp)
+
+# export to match data to file
+with open('matched.csv', 'w') as f:
+	for m in jsondata:
+		name = m["name"]
+		query = m["query"]
+		url = "https://app.datadoghq.com/monitors/" + str(m["id"])
+
+		# extract the text within the 'exclude' parentheses
+		match=re.findall(r"\.exclude\((.*?\)).",query)
+		if match:
+			f.write(name + str(",") + url + ",\"" + str(match).replace("\"","\"\"") + str("\"\n"))
